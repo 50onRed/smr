6ff5d988ea578becab8082c9e2001b82c675e198
Ivan Dyedov <idyedov@50onred.com>
2013-12-22 03:26:04 -0500

    make map workers abort more gracefully

--- smr/map.py
+++ smr/map.py
@@ -19,24 +19,28 @@ def main():
 
     s3conn = boto.connect_s3(config.AWS_ACCESS_KEY, config.AWS_SECRET_KEY)
     bucket = s3conn.get_bucket(config.S3_BUCKET_NAME)
-    for file_name in sys.stdin:
-        k = Key(bucket)
-        file_name = file_name.rstrip() # remove trailing linebreak
-        k.key = file_name
-        temp_file, temp_filename = tempfile.mkstemp()
-        tries = 0
-        while True:
+    try:
+        for file_name in sys.stdin:
+            k = Key(bucket)
+            file_name = file_name.rstrip() # remove trailing linebreak
+            k.key = file_name
+            temp_file, temp_filename = tempfile.mkstemp()
+            tries = 0
+            while True:
+                try:
+                    k.get_contents_to_filename(temp_filename)
+                except Exception as e:
+                    tries += 1
+                    if tries >= config.DOWNLOAD_RETRIES:
+                        logging.error("could not download file %s after %d tries", file_name, tries)
+                        raise e
+                else:
+                    break
             try:
-                k.get_contents_to_filename(temp_filename)
-            except Exception as e:
-                tries += 1
-                if tries >= config.DOWNLOAD_RETRIES:
-                    logging.error("could not download file %s after %d tries", file_name, tries)
-                    raise e
-            else:
-                break
-        try:
-            config.MAP_FUNC(temp_filename)
-        finally:
-            os.close(temp_file)
-            os.unlink(temp_filename)
+                config.MAP_FUNC(temp_filename)
+            finally:
+                os.close(temp_file)
+                os.unlink(temp_filename)
+    except (KeyboardInterrupt, SystemExit):
+        logging.error("map worker %d aborted", os.getpid())
+        sys.exit(1)
--- smr/reduce.py
+++ smr/reduce.py
@@ -17,5 +17,6 @@ def main():
             config.REDUCE_FUNC(result.rstrip()) # remove trailing linebreak
         config.OUTPUT_RESULTS_FUNC()
     except (KeyboardInterrupt, SystemExit):
+        # we want to output results even if user aborted
         config.OUTPUT_RESULTS_FUNC()
 
