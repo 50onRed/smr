5ebf34e1c572e5db9012af4228eaca2a8461b8d9
Ivan Dyedov <idyedov@50onred.com>
2014-01-18 13:50:47 -0500

    add some extra debug logging to smr-reduce

--- smr/reduce.py
+++ smr/reduce.py
@@ -13,7 +13,9 @@ def main():
 
     try:
         for result in iter(sys.stdin.readline, ""):
-            config.REDUCE_FUNC(result.rstrip()) # remove trailing linebreak
+            result = result.rstrip() # remove trailing linebreak
+            logging.debug("smr-reduce got %s", result)
+            config.REDUCE_FUNC(result)
     except (KeyboardInterrupt, SystemExit):
         # we want to output results even if user aborted
         config.OUTPUT_RESULTS_FUNC()
