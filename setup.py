3c6f0f03c3af15f3c87bedecb3ee30c5e7d5251b
Ivan Dyedov <idyedov@50onred.com>
2014-04-17 14:15:47 -0400

    upgrade psutil

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email='ivan@dyedov.com',
     url='https://github.com/idyedov/smr',
     packages=['smr'],
-    install_requires=['boto>=2.27.0', 'paramiko>=1.13.0', 'psutil>=2.0.0'],
+    install_requires=['boto>=2.27.0', 'paramiko>=1.13.0', 'psutil>=2.1.0'],
     entry_points={
         'console_scripts': [
             'smr = smr.main:main',
